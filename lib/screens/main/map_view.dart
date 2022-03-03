be8f80572d99c797364827ad01ff90bf27828123
Adam <61555703+akunsecured@users.noreply.github.com>
2022-03-03 20:13:49 +0100

    Adding Google Maps to the project

--- lib/screens/main/map_view.dart
+++ lib/screens/main/map_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapView extends StatefulWidget {
   @override
@@ -6,12 +7,35 @@ class MapView extends StatefulWidget {
 }
 
 class _MapViewState extends State<MapView> {
+  final Map<String, Marker> _markers = {};
+  late final GoogleMapController _mapController;
+
+  final LatLng _center = const LatLng(47.4733413,19.0597962);
+
+  void _onMapCreated(GoogleMapController controller) {
+    _mapController = controller;
+    setState(() {
+      _markers.clear();
+      _markers["0"] = Marker(
+        markerId: MarkerId("0"),
+        position: _center
+      );
+    });
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Align(
         alignment: Alignment.center,
-        child: Text('Map'),
+        child: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 17.0
+          ),
+          markers: _markers.values.toSet(),
+        ),
       ),
     );
   }
